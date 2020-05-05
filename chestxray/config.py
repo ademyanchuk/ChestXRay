cea7ab0f04919cb4d865237d96c6ff5e46b9d4dd
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-05-05 07:42:52 +0200

    Add project and model paths

--- chestxray/config.py
+++ chestxray/config.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 DATA_PATH = Path(os.getenv("DATA_PATH"))
 
+PROJ_PATH = DATA_PATH.parent
+
+MODEL_PATH = PROJ_PATH / "models"
+
 PANDA_PATH = DATA_PATH / "Panda"
 PANDA_IMGS = PANDA_PATH / "train_images"
 PANDA_MASKS = PANDA_PATH / "train_label_masks"
