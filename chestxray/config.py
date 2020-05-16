a1beb487fab50e22536e6da1f04cf19e45a884af
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-05-16 19:45:33 +0200

    Update config

--- chestxray/config.py
+++ chestxray/config.py
@@ -16,7 +16,7 @@ PANDA_IMGS = PANDA_PATH / "train_images"
 PANDA_MASKS = PANDA_PATH / "train_label_masks"
 TRAIN_CSV = PANDA_PATH / "train.csv"
 
-TILES_IMGS = PANDA_PATH / "tiles32x128x1"
+TILES_IMGS = PANDA_PATH / "tiles144x64x1"
 
 
 # PANDA competition config
@@ -32,22 +32,25 @@ class CFG:
     target_col = "isup_grade"
     tiff_layer = 1
     stoch_sample = True
-    num_tiles = 36
-    tile_sz = 128
-    batch_size = 16
+    num_tiles = 144
+    tile_sz = 64
+    batch_size = 8
     use_lazy = True
     aug_type = "light"
     # model
-    finetune = "1stage"
-    model_cls = "deep"
-    schedule_type = "one_cycle"
+    finetune = False  # or "1stage"
+    model_cls = "deep"  # "one_layer" or "deep"
+    schedule_type = "cawr"  # "one_cycle", "reduce_on_plateau" or "cawr"
+    cawr_T = 1
+    cawr_Tmult = 2
     # loss
     loss = "ls_soft_ce"
     # optim
-    lr = 5e-5
+    optim = "sgd"  # "adam" or "sgd"
+    lr = 0.1 * batch_size / 512 if optim == "sgd" else 5e-5
     # schedule
     rlopp = 3  # learnig rate on plateu scheduler patience
     # training
-    epoch = 70
+    epoch = 50
     n_fold = 4
     use_amp = True
