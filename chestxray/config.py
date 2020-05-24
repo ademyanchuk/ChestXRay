c526217e795a8a086ea7ba01e5cdb21a1740eb1f
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-05-24 20:14:35 +0200

    Refactor schedule part

--- chestxray/config.py
+++ chestxray/config.py
@@ -22,11 +22,11 @@ TILES_IMGS = PANDA_PATH / "tiles36x128x1"  # "tiles144x64x1" or "tiles36x128x1"
 # PANDA competition config
 class CFG:
     # overall
-    debug = True
+    debug = False
     seed = 1982
     # data
-    img_height = 1024
-    img_width = 1024
+    img_height = 256
+    img_width = 256
     target_size = 6
     img_id_col = "image_id"
     target_col = "isup_grade"
@@ -41,17 +41,17 @@ class CFG:
     # model
     finetune = False  # or "1stage"
     model_cls = "one_layer"  # "one_layer" or "deep"
-    schedule_type = "none"  # "one_cycle", "reduce_on_plateau" or "cawr"
-    cawr_T = 1
-    cawr_Tmult = 2
     # loss
-    loss = "cce"  # "cce" or "ls_soft_ce"
+    loss = "ls_soft_ce"  # "cce" or "ls_soft_ce"
     # optim
     optim = "adam"  # "adam", "sgd" or "radam"
-    lr = 0.001 if optim == "sgd" else 1e-4
+    lr = 0.001 if optim == "sgd" else 3e-4
     # schedule
+    schedule_type = "reduce_on_plateau"  # "one_cycle", "reduce_on_plateau" or "cawr"
+    cawr_T = 1
+    cawr_Tmult = 2
     rlopp = 3  # learnig rate on plateu scheduler patience
     # training
-    epoch = 70
+    epoch = 60
     n_fold = 4
     use_amp = True
