f63aee238f4b18927b86b477cb4ec8118a37dfed
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-06-04 20:46:24 +0200

    Debug conf for train/eval script

--- chestxray/config.py
+++ chestxray/config.py
@@ -22,7 +22,7 @@ TILES_IMGS = PANDA_PATH / "tiles36x128x1"  # "tiles144x64x1" or "tiles36x128x1"
 # PANDA competition config
 class CFG:
     # overall
-    debug = False
+    debug = True
     seed = 1982
     # data
     img_height = 256
@@ -32,26 +32,33 @@ class CFG:
     target_col = "isup_grade"
     tiff_layer = 1
     stoch_sample = True
-    num_tiles = 16
+    num_tiles = 32
     tile_sz = 256
     batch_size = 4
-    accum_step = 16  # effective batch size will be batch_size * accum_step
-    dataset = "hdf5"  # "patch", "tiles", "lazy", "hdf5"
-    aug_type = "light"  # "light" or "heavy"
+    accum_step = 1  # effective batch size will be batch_size * accum_step
+    dataset = "patch"  # "patch", "tiles", "lazy", "hdf5"
+    aug_type = "heavy"  # "light" or "heavy"
     # model
+    arch = "resnet50"  # "resnet34", "resnet50", "bitM"
     finetune = False  # or "1stage"
     model_cls = "one_layer"  # "one_layer" or "deep"
     # loss
-    loss = "ls_soft_ce"  # "cce" or "ls_soft_ce"
+    loss = "cce"  # "cce" or "ls_soft_ce"
     # optim
-    optim = "adam"  # "adam", "sgd" or "radam"
-    lr = 0.001 if optim == "sgd" else 3e-4
+    optim = "radam"  # "adam", "sgd" or "radam"
+    lr = 0.001 if optim == "sgd" else 1e-4
     # schedule
-    schedule_type = "reduce_on_plateau"  # "one_cycle", "reduce_on_plateau" or "cawr"
+    schedule_type = "none"  # "one_cycle", "reduce_on_plateau" or "cawr"
     cawr_T = 1
     cawr_Tmult = 2
-    rlopp = 3  # learnig rate on plateu scheduler patience
+    rlopp = 1  # learnig rate on plateu scheduler patience
     # training
-    epoch = 60
+    resume = False
+    prev_exp = None
+    from_epoch = None
+    stage = None
+    epoch = 35
     n_fold = 4
     use_amp = True
+    # Experiment
+    descript = "Debug"
