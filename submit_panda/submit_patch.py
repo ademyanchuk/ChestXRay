fa48efa6fcab7099f04a8cbf3fafcf87178a5e66
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-06-06 13:50:35 +0200

    Update for current setup

--- submit_panda/submit_patch.py
+++ submit_panda/submit_patch.py
@@ -30,28 +30,34 @@ class CFG:
     stoch_sample = True
     num_tiles = 32
     tile_sz = 256
-    batch_size = 4
+    batch_size = 2
     accum_step = 2  # effective batch size will be batch_size * accum_step
     dataset = "patch"  # "patch", "tiles", "lazy", "hdf5"
-    aug_type = "heavy"  # "light" or "heavy"
+    aug_type = "light"  # "light" or "heavy"
     # model
-    arch = "resnet34"  # "resnet34", "resnet50"
+    arch = "resnet50"  # "resnet34", "resnet50", "bitM"
     finetune = False  # or "1stage"
     model_cls = "one_layer"  # "one_layer" or "deep"
     # loss
     loss = "ls_soft_ce"  # "cce" or "ls_soft_ce"
     # optim
     optim = "radam"  # "adam", "sgd" or "radam"
-    lr = 0.001 if optim == "sgd" else 5e-5
+    lr = 1e-3 if optim == "sgd" else 1e-4
     # schedule
     schedule_type = "none"  # "one_cycle", "reduce_on_plateau" or "cawr"
     cawr_T = 1
     cawr_Tmult = 2
     rlopp = 1  # learnig rate on plateu scheduler patience
     # training
-    epoch = 30
+    resume = False
+    prev_exp = "None"
+    from_epoch = 0
+    stage = 0
+    epoch = 20
     n_fold = 4
     use_amp = True
+    # Experiment
+    descript = "Try overfit with basic config"
 
 
 # Datasets
