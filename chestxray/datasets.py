6e4e4cece6183790010c66ff5a70ae63ebc6eadb
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-06-08 23:18:53 +0200

    Update heavy to add one aug at a time

--- chestxray/datasets.py
+++ chestxray/datasets.py
@@ -26,49 +26,56 @@ BIT_STD = [0.5, 0.5, 0.5]
 augs_dict = {
     "heavy": A.Compose(
         [
-            A.OneOf(
-                [
-                    A.ShiftScaleRotate(
-                        shift_limit=0.05,
-                        scale_limit=0.1,
-                        rotate_limit=15,
-                        border_mode=cv2.BORDER_CONSTANT,
-                        value=(255, 255, 255),
-                    ),
-                    A.OpticalDistortion(
-                        distort_limit=0.11,
-                        shift_limit=0.15,
-                        border_mode=cv2.BORDER_CONSTANT,
-                        value=(255, 255, 255),
-                    ),
-                    A.NoOp(),
-                ]
+            A.Flip(),
+            A.ShiftScaleRotate(
+                shift_limit=0.05,
+                scale_limit=0.1,
+                rotate_limit=15,
+                border_mode=cv2.BORDER_CONSTANT,
+                value=(255, 255, 255),
             ),
-            A.RandomSizedCrop(
-                min_max_height=(int(CFG.img_height * 0.75), CFG.img_height),
-                height=CFG.img_height,
-                width=CFG.img_width,
-                p=0.3,
-            ),
-            A.OneOf(
-                [
-                    A.RandomBrightnessContrast(
-                        brightness_limit=0.3, contrast_limit=0.3
-                    ),
-                    A.RandomGamma(gamma_limit=(50, 150)),
-                    A.NoOp(),
-                ]
-            ),
-            A.OneOf(
-                [
-                    A.RGBShift(r_shift_limit=20, b_shift_limit=15, g_shift_limit=15),
-                    A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=5),
-                    A.NoOp(),
-                ]
-            ),
-            A.OneOf([A.CLAHE(), A.NoOp()]),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+            # A.OneOf(
+            #     [
+            #         A.ShiftScaleRotate(
+            #             shift_limit=0.05,
+            #             scale_limit=0.1,
+            #             rotate_limit=15,
+            #             border_mode=cv2.BORDER_CONSTANT,
+            #             value=(255, 255, 255),
+            #         ),
+            #         A.OpticalDistortion(
+            #             distort_limit=0.11,
+            #             shift_limit=0.15,
+            #             border_mode=cv2.BORDER_CONSTANT,
+            #             value=(255, 255, 255),
+            #         ),
+            #         A.NoOp(),
+            #     ]
+            # ),
+            # A.RandomSizedCrop(
+            #     min_max_height=(int(CFG.img_height * 0.75), CFG.img_height),
+            #     height=CFG.img_height,
+            #     width=CFG.img_width,
+            #     p=0.3,
+            # ),
+            # A.OneOf(
+            #     [
+            #         A.RandomBrightnessContrast(
+            #             brightness_limit=0.3, contrast_limit=0.3
+            #         ),
+            #         A.RandomGamma(gamma_limit=(50, 150)),
+            #         A.NoOp(),
+            #     ]
+            # ),
+            # A.OneOf(
+            #     [
+            #         A.RGBShift(r_shift_limit=20, b_shift_limit=15, g_shift_limit=15),
+            #         A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=5),
+            #         A.NoOp(),
+            #     ]
+            # ),
+            # A.OneOf([A.CLAHE(), A.NoOp()]),
             # This transformation first / 255. -> scale to [0,1] and
             # then - mean and / by std
             # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],),
