326201192d6e7e3c1f232629083c3fc09e4a0f64
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-06-16 22:53:52 +0200

    Update for new rn34+bce baseline

--- chestxray/config.py
+++ chestxray/config.py
@@ -25,16 +25,16 @@ class CFG:
     debug = False
     seed = 1982
     # data
-    img_height = 256
-    img_width = 256
+    img_height = 224
+    img_width = 224
     target_size = 6
     img_id_col = "image_id"
     target_col = "isup_grade"
     tiff_layer = 1
     stoch_sample = True
-    num_tiles = 36
-    tile_sz = 256
-    batch_size = 4
+    num_tiles = 32
+    tile_sz = 224
+    batch_size = 8
     accum_step = 1  # effective batch size will be batch_size * accum_step
     dataset = "patch"  # "patch", "tiles", "lazy", "hdf5"
     aug_type = "light"  # "light" or "heavy"
@@ -45,7 +45,7 @@ class CFG:
     model_cls = "one_layer"  # "one_layer" or "deep"
     pre_init_fc_bias = True
     # loss
-    loss = "huber"  # "cce" or "ls_soft_ce", "ohem", "bce", "huber"
+    loss = "bce"  # "cce" or "ls_soft_ce", "ohem", "bce"
     # optim
     optim = "radam"  # "adam", "sgd" or "radam"
     lr = 1e-3 if optim == "sgd" else 3e-4
@@ -59,8 +59,8 @@ class CFG:
     prev_exp = "None"
     from_epoch = 0
     stage = 0
-    epoch = 33
+    epoch = 40
     n_fold = 4
     use_amp = True
     # Experiment
-    descript = "huber + rn34 + one cycle + 256x36"
+    descript = "bce + rn34 + one cycle + 224x32"
