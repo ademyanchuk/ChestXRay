874c1e8fb2d44ddf37d6122b6a2ef066dbd6071b
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-07-18 20:16:16 +0200

    Add focal loss

--- chestxray/losses.py
+++ chestxray/losses.py
@@ -154,3 +154,28 @@ class TopkBCEWithLogitsLoss(nn.Module):
         else:
             hard_loss, _ = torch.topk(loss, int(self.top_k * loss.shape[0]))
             return torch.mean(hard_loss)
+
+
+# from https://www.kaggle.com/c/tgs-salt-identification-challenge/discussion/65938
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=True, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(
+                inputs, targets, reduction="none"
+            )
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduction="none")
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
