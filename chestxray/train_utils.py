fa1a4b214a8de98bfa128ed35ab49c68b983f451
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-08-08 10:11:52 +0200

    Update utility

--- chestxray/train_utils.py
+++ chestxray/train_utils.py
@@ -1,5 +1,5 @@
 def compute_preds(outputs, labels, task_type):
-    if task_type == "bce":
+    if task_type in ["bce", "focal"]:  # here is bce version of focal loss
         preds = outputs.detach().sigmoid().sum(1)
         if labels is not None:
             targs = labels.sum(1)
